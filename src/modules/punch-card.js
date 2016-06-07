0970c6dcc011f635c4db5ea8ae9d30b311c7dfe0
Leendert van Doorn <l.b.vandoorn@student.tudelft.nl>
2016-06-07 16:46:36 +0200

    Add labels to axes

--- src/modules/punch-card.js
+++ src/modules/punch-card.js
@@ -6,28 +6,29 @@ define(function () {
         name: "punch-card",
         title: "Code review punch card",
         parentSelector: "#personal-modules",
-        xAxis: false,
-        yAxis: false,
+        size: "m6",
+        xAxis: true,
+        yAxis: true,
+        xAxisLine: false,
+        yAxisLine: false,
+        xAxisTicks: false,
+        yAxisTicks: false,
+        xAxisScale: function () {
+            var axisScale = d3.scale.linear().domain([0, 23]);
+            return d3.svg.axis().ticks(24).scale(axisScale);
+        },
+        yAxisScale: function () {
+            var axisScale = d3.scale.linear().domain([0, 6]);
+            return d3.svg.axis()
+                .ticks(7)
+                .tickFormat(function (d, i) {
+                    return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d];
+                })
+                .scale(axisScale);
+        },
         body: function () {
             var g = d3.select(document.createElementNS(d3.ns.prefix.svg, "g"));
-            var w = 720,
-                h = 350,
-                pad = 50,
-                padTop = 10,
-                padBottom = 50,
-            left_pad = 20;
-            var x = d3.scale.linear().domain([0, 23]).range([left_pad, w - pad]),
-    y = d3.scale.linear().domain([0, 6]).range([pad, h - pad * 2]);
-            var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-                yAxis = d3.svg.axis().scale(y).orient("left");
             
-            g.data([1, 2, 3]).attr("class", "axis")
-    .attr("transform", "translate(0, " + (h - pad) + ")").call(xAxis);
-            
-            var circle = g.append("circle")
-            .attr("cx", 30)
-            .attr("cy", 30)
-            .attr("r", 20);
 
             return g;
         }
