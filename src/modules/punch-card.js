dce1c43c93b625b9b504c61768bd793cd8342a12
Leendert van Doorn <l.b.vandoorn@student.tudelft.nl>
2016-06-07 14:29:44 +0200

    Add some dummy visualization

--- src/modules/punch-card.js
+++ src/modules/punch-card.js
@@ -10,9 +10,24 @@ define(function () {
         yAxis: false,
         body: function () {
             var g = d3.select(document.createElementNS(d3.ns.prefix.svg, "g"));
+            var w = 720,
+                h = 350,
+                pad = 50,
+                padTop = 10,
+                padBottom = 50,
+            left_pad = 20;
+            var x = d3.scale.linear().domain([0, 23]).range([left_pad, w - pad]),
+    y = d3.scale.linear().domain([0, 6]).range([pad, h - pad * 2]);
+            var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+                yAxis = d3.svg.axis().scale(y).orient("left");
             
-
-
+            g.data([1, 2, 3]).attr("class", "axis")
+    .attr("transform", "translate(0, " + (h - pad) + ")").call(xAxis);
+            
+            var circle = g.append("circle")
+            .attr("cx", 30)
+            .attr("cy", 30)
+            .attr("r", 20);
 
             return g;
         }
