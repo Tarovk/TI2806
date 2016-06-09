2cb28b181f7ae190f645cb26d639bcb276069256
BorekBeker <bekerborek@gmail.com>
2016-06-09 12:34:11 +0200

    Added pr info to punch card

--- src/aggregators/punchCardAggregator.js
+++ src/aggregators/punchCardAggregator.js
@@ -1,15 +1,14 @@
 /*exported Graph1Aggregator*/
-/*globals octopeerService, RSVP, ObjectResolver*/
+/*globals octopeerService, RSVP, ObjectResolver, PullRequestResolver*/
 //https://docs.google.com/document/d/1QUu1MP9uVMH9VlpEFx2SG99j9_TgxlhHo38_bgkUNKk/edit?usp=sharing
 /*jshint unused: false*/
 function PunchCardAggregator(userName) {
     "use strict";
-    var promise;
+    var promise, pullRequestResolver = new PullRequestResolver();
     
     function getEndEvents(startEvents) {
         return octopeerService.getSemanticEventsFromUser(userName, 402)
             .then(function (endEvents) {
-            console.log(endEvents);
                 return {
                     "startEvents": startEvents,
                     "endEvents": endEvents
@@ -22,20 +21,19 @@ function PunchCardAggregator(userName) {
             counter = 0;
         startEndEvents.startEvents.forEach(function (se) {
             var endDate;
-            console.log(startEndEvents.endEvents.length);
             if (startEndEvents.endEvents.length > counter) {
                 endDate = startEndEvents.endEvents[counter].created_at;
-                console.log(endDate);
-                counter++;
+                counter += 1;
             } else {
                 endDate = se.created_at;
             }
+            pullRequestResolver.resolveSinglePullRequest(se.session.pull_request);
             graphObject.push({
                 "start": se.created_at,
-                "end": endDate
+                "end": endDate,
+                "session": se.session
             });
         });
-        console.log(graphObject);
         return graphObject;
     }
     
