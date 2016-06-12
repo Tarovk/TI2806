f1f976a7f5737f38a978a0342131cdea539c1505
BorekBeker <bekerborek@gmail.com>
2016-06-12 10:31:10 +0200

    Fixed test

--- spec/modules/behaviour-m4-modules_test.js
+++ spec/modules/behaviour-m4-modules_test.js
@@ -1,9 +1,9 @@
 define(["src/modules/behaviour-m4-modules"], function (module) {
-	var data = {"keystrokes":20762,
+	var data = [{"keystrokes":20762,
         "clicks":{"total":513, "merge":113, "close":113},
         "scrolls":932,
         "comments": {"pr":94,"inline":153,"edits":12}
-    };
+    }];
     var div = module.prebody();
     module.body(data);
     describe('Test suite for the behaviour-m4-modules module', function () {
