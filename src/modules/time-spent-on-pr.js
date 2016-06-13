7df66e5d7b2d2de842bf80a77baa3a720ddbb55e
BorekBeker <bekerborek@gmail.com>
2016-06-13 12:11:37 +0200

    Added real data to timespent and lines of code graphs

--- src/modules/time-spent-on-pr.js
+++ src/modules/time-spent-on-pr.js
@@ -6,29 +6,7 @@ define(function () {
             h = 350,
             pad = 50,
             padTop = 10,
-            padBottom = 50,
-            timeData = [
-                {"x":0, "y":35},
-                {"x":1, "y":5},
-                {"x":2, "y":17},
-                {"x":3, "y":28},
-                {"x":4, "y":122},
-                {"x":5, "y":2},
-                {"x":6, "y":3},
-                {"x":7, "y":75},
-                {"x":8, "y":40},
-                {"x":9, "y":34},
-                {"x":10, "y":72},
-                {"x":11, "y":24},
-                {"x":12, "y":41},
-                {"x":13, "y":34},
-                {"x":14, "y":72},
-                {"x":15, "y":27},
-                {"x":16, "y":42},
-                {"x":17, "y":137},
-                {"x":18, "y":57},
-                {"x":19, "y":90}
-            ];
+            padBottom = 50;
     return {
     	name: "time-spent-on-pr",
         title: "Time spent on pr",
@@ -40,15 +18,11 @@ define(function () {
         xAxisTicks: false,
         yAxisTicks: true,
         xAxisLabelRotation: 65,
-        xAxisScale: function() { 
+        xAxisFitFunction: function() { 
             var axisScale = d3.scale.ordinal()
-                .domain([
-                    "pr0", "pr1", "pr2", "pr3",
-                    "pr4", "pr5", "pr6", "pr7",
-                    "pr8", "pr9", "pr10", "pr11",
-                    "pr12", "pr13", "pr14", "pr15",
-                    "pr16", "pr17", "pr18", "pr19"
-                ])
+                .domain(timeData.map(function (pr) {
+                    return pr.x;
+                }))
                 .rangePoints([0.35*50, 720-2.3*50]);
             return d3.svg.axis().scale(axisScale);
         },
@@ -65,7 +39,12 @@ define(function () {
                 "text":"Time spent on pull request"
             }
         ],
-        body: function () {
+        data: [{
+            "serviceCall": function () { return new TimeSpentSizePrAggregator(globalUserName); },
+            "required": true
+        }],
+        body: function (data) {
+            timeData = data[0].timeSpent;
             var xTimeScale = d3.scale.linear()
                 .domain([0,timeData.length])
                 .range([pad,w-pad]),
